c8ab995dd7920bbaef29e45d9dd27bacf2f8cd46
pmaryon <maryonpawel@gmail.com>
2025-11-24 12:23:28 +0000

    books controller actions written

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,35 @@
 class BooksController < ApplicationController
+  def index
+    @filters = {
+      title:       params[:title],
+      author:      params[:author],
+      tag:         params[:tag],
+      series_name: params[:series_name]
+    }
+
+    @books = Book.includes(:author, :tag).order(:title)
+
+    if @filters[:title].present?
+      @books = @books.with_title_like(@filters[:title])
+    end
+
+    if @filters[:author].present?
+      @books = @books.with_author_like(@filters[:author])
+    end
+
+    if @filters[:tag].present?
+      @books = @books.with_tag_like(@filters[:tag])
+    end
+
+    if @filters[:series_name].present?
+      @books = @books.where(
+        "LOWER(series_name) LIKE ?",
+        "%#{@filters[:series_name].downcase}%"
+      )
+    end
+  end
+
+  def show
+    @book = Book.includes(:author, :tag).find(params[:id])
+  end
 end
